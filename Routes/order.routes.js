e426bca584bf1051b167f4d7017a725f8bbe11b7
MohamedAwara <mohamedaymanawara@gmail.com>
2025-01-17 16:27:31 +0200

    Orders SEF Version 1.0.0

---
+++ Routes/order.routes.js
@@ -0,0 +1,22 @@
+import express from "express";
+import {
+    createOrder,
+    deleteOrderById,
+    getAllOrders,
+    getOrderById,
+    updateOrder,
+} from "../Controllers/order.controller.js";
+import { authentication, restrictTo } from "../Middlewares/auth.middleware.js";
+
+let orderRouter = express.Router();
+
+orderRouter.route("/").get(getAllOrders);
+orderRouter.route("/orderId").get(getOrderById);
+orderRouter.use(authentication);
+orderRouter.route("/:productId").post(createOrder);
+orderRouter
+    .route("/:orderId")
+    .patch(updateOrder)
+    .delete(restrictTo("admin"), deleteOrderById);
+
+export default orderRouter;
